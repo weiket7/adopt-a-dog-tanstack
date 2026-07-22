ce3245106ffbde01f84a321ad2e150d56734358b
Wei Ket <weiketong@getgo.sg>
2026-07-22 14:06:03 +0800

    Remove dead Icon.Heart and Icon.Cake entries
    ADO-12

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -17,11 +17,6 @@ export const Icon = {
       <path d="m20 20-3.5-3.5" />
     </svg>
   ),
-  Heart: () => (
-    <svg viewBox="0 0 24 24">
-      <path d="M12 21s-7-4.5-9.5-9C.5 8 3 4 7 4c2 0 3.5 1 5 3 1.5-2 3-3 5-3 4 0 6.5 4 4.5 8-2.5 4.5-9.5 9-9.5 9z" />
-    </svg>
-  ),
   Check: () => (
     <svg
       viewBox="0 0 24 24"
@@ -77,20 +72,6 @@ export const Icon = {
       strokeLinecap="round"
     >
       <path d="M6 6l12 12M18 6 6 18" />
-    </svg>
-  ),
-  Cake: () => (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M4 21V11a3 3 0 0 1 3-3h10a3 3 0 0 1 3 3v10z" />
-      <path d="M4 16c2 0 2-2 4-2s2 2 4 2 2-2 4-2 2 2 4 2" />
-      <path d="M12 4v4M10 4a2 2 0 1 1 4 0c0 1-1 2-2 2s-2-1-2-2z" />
     </svg>
   ),
   Send: () => (
